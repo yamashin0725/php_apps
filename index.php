6d4c0a29047081ba4079478bc3b2ba14dc33e391
ShinichiroYamazaki <yamashin6225@gmail.com>
2018-05-07 12:46:16 +0000

    php_lessons~#19

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
 <?php
-
-# Hello comment
-echo "hello from the TOP!";
-echo "hello from the TOP! again";
-
+    
+    class User {
+        //property
+        public $name;
+        
+        //constructor
+        public function __construct($name) {
+            $this->name = $name;
+        }
+        
+        //method
+        public function sayHi(){
+            echo "hi, i am $this->name!";
+        }
+    
+    }
+    
+    $tom = new User("Tom");
+    $bob = new User("Bob");
+    
+    echo $tom->name;
+    $bob->sayHi();
+    
+    $colors = ["red", "pink", "blue"];
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-<body>
-    <p>Hello World <?php echo " from PHP"; ?></p>
-</body>
-</html>
+<ul>
+    <?php foreach ($colors as $value) : ?>
+    <li><?php echo "$value "; ?></li>
+    <?php endforeach; ?>
+</ul>
+
--- sample.php
+++ sample.php
@@ -1,2 +1,3 @@
 <?PHP
     echo "helloworld";
+?>
