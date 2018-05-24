2789f56b8619de61617e8d019e50f6ca95fede67
Ubuntu <ubuntu@yamax.c.yamax-project.internal>
2018-05-24 12:22:10 +0000

    first commit from aws cloud9

--- mysql_lesson/myapp.sql
+++ mysql_lesson/myapp.sql
@@ -2,36 +2,16 @@ drop table if exists posts;
 create table posts (
     id int unsigned primary key auto_increment,
     title varchar(255),
-    body text
+    body text,
+    created datetime default current_timestamp,
+    updated datetime default current_timestamp on update current_timestamp
 );
 
-drop table if exists logs;
-create table logs (
-    id int unsigned primary key auto_increment,
-    msg varchar(255)
-);
-
-
-drop trigger if exists posts_update_trigger;
-drop trigger if exists posts_insert_trigger;
-delimiter //
-create trigger posts_update_trigger after update on posts for each row
-    begin
-        insert into logs (msg) values ('post updated!');
-        insert into logs (msg) values (concat(old.title, '->', new.title));
-    end;
-//
-
-delimiter ;
-
-insert into posts(title, body) values ('title 1', 'body 1');
-insert into posts(title, body) values ('title 2', 'body 2');
-insert into posts(title, body) values ('title 3', 'body 3');
-
-update posts set title = 'title 2 updated' where id = 2;
+insert into posts (title, body) values ('title 1', 'body 1');
+insert into posts (title, body) values ('title 2', 'body 2');
+insert into posts (title, body) values ('title 3', 'body 3');
 
 select * from posts;
-select * from logs;
 
 
 -- show triggers \G
